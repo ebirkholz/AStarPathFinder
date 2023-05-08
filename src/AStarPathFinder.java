6316cea37d417068dcf3283da60eeedb203030f6
Erick <erick.silva@datalakers.com.br>
2023-05-07 22:37:44 -0300

    atualizacao no codigo com heuristica

--- src/AStarPathFinder.java
+++ src/AStarPathFinder.java
@@ -3,123 +3,111 @@ import java.util.*;
 public class AStarPathFinder
 {
     // Tamanho da Matriz teste
-    public int M = 70;
-    public int N = 100;
-
-    // Arrays com os 4 movimentos possiveis a partir de uma celula
-    private static final int row[] = { -1, 0, 0, 1 };
-    private static final int col[] = { 0, -1, 1, 0 };
-
+    public int M = 70; //70
+    public int N = 100; //100
 
 
     // Função que verifica se é possível ir para a nova posição (row, col)
     // a partir da posição atual. Ela retorna falso se (row, col) nova
     // não for valido (ex fora do range da matriz) ou o valor é 0 (parede)
     // ou já esta na lista de visitados.
-    private boolean isValid(int mat[][],  boolean visited[][], int row, int col)
+    public boolean isValid(int mat[][],  boolean visited[][], int x, int y)
     {
-        return (row >= 0) && (row < M) && (col >= 0) && (col < N)
-                && mat[row][col]==0 && !visited[row][col];
+        return (x >= 0) && (x < M) && (y >= 0) && (y < N)
+                && mat[x][y]==0 && !visited[x][y];
     }
 
     // Busca o menor caminho possivel na matriz com inicio em
     // celula (i, j) e destino cell (x, y)
-    public void AStarPathFinder(int mat[][],int altura, int largura,int i, int j, int x, int y)
+    public void AStarPathFinder(int mat[][],int altura, int largura,int inicioX, int inicioY, int fimX, int fimY)
     {
         this.M = altura;
         this.N = largura;
-        Node destino = new Node(i,j,0,null);
         // matriz que vai armazenar celular já visitadas
-        boolean[][] visited = new boolean[M][N];
-
-        //Cria uma lista de Nodos para reconstruir o caminho.
-        LinkedList<Node> caminho = new LinkedList<Node>();
-
-        //Cria uma lista de visitados
-        LinkedList<Node> visitados = new LinkedList<Node>();
+        boolean[][] visitados = new boolean[M][N];
 
         // cria uma PILHA vazia
-        Queue<Node> fila = new LinkedList<>();
+        PriorityQueue<Node> fila = new PriorityQueue<Node>();
 
         // marca a celula atual como visitada e empilha na pilha
-        visited[i][j] = true;
-        fila.add(new Node(i, j, 0, null));
-        visitados.add(new Node(i,j, 0, null));
 
-        // armazena o maior caminho possível desde o inicio até o destino
-        int min_dist = Integer.MAX_VALUE;
+        fila.add(new Node(inicioX, inicioY, 0, 0,null));
+
 
         // roda até a pilha não estar vazia
         while (!fila.isEmpty())
         {
+            System.out.println("entrou");
             //tira o Node do top e processa ele
-            Node node = fila.poll();
+            Node atual = fila.poll();
+            visitados[inicioX][inicioY] = true;
 
-            // (i, j) representa a celula atual e armazena na variavel dist
-            // a menor distancia desde o começo
-            i = node.x;
-            j = node.y;
-            int dist = node.dist;
 
             // se a celula destino é achada, atualiza a variavel min_dist e para.
-            if (i == x && j == y)
+            if (atual.x == fimX && atual.y == fimY)
             {
-                min_dist = dist;
-                destino = new Node(i,j,min_dist,node.parent);
-                break;
+                System.out.println("achou");
+                printaCaminho(atual);
+                return;
             }
 
             // checa pelas 4 possibilidades de movimento a partir da celula atual
             // e empilha cada movimento valido (usa o metodo para validar)
-            for (int k = 0; k < 4; k++)
+            for (int i = -1; i <= 1; i++)
             {
-                // checa se é possível ir para a posição
-                // (i + row[k], j + col[k]) a partir da celula atual
-                if (isValid(mat, visited, i + row[k], j + col[k]))
-                {
-                    // marca a proxima possivel como visitada e empilha ela para ir pro loop tbm
-                    visited[i + row[k]][j + col[k]] = true;
-                    //TODO Antes de empilhar, validar heuristica
-                    fila.add(new Node(i + row[k], j + col[k], dist + 1, node));
-                    visitados.add(new Node(i + row[k], j + col[k], dist + 1, node));
+                for (int j = -1; j <= 1 ; j++) {
+                    //Pula o nodo atual
+                    if(i==0 && j==0){
+                        continue;
+                    }
+                    int x = atual.x + i;
+                    int y = atual.y + j;
+
+                    if (isValid(mat, visitados, x, y))
+                    {
+                        // Calcula heuristica
+                        int heuristica = Math.abs(x - fimX) + Math.abs(y - fimY);
+                        // Create new node and add it to the queue
+                        fila.add(new Node(x, y, atual.dist + 1, heuristica, atual));
+                        System.out.println("adiciounou --> X:"+x+" Y:"+y);
+                    }
                 }
             }
         }
 
-        if (min_dist != Integer.MAX_VALUE)
-        {
-            System.out.println("O caminho mais curto da origem até o destino " +
-                    "tem o tamanho: " + min_dist);
-
-
-            while(destino.parent != null) {
-                System.out.println("X:"+destino.x + " Y:"+destino.y);
-                destino = destino.parent;
-            }
-        }
-        else
-        {
-            System.out.println("A partir do inicio especificado não é possível chegar ao destino");
-        }
+        System.out.println("A partir do inicio especificado não é possível chegar ao destino");
     }
 
+    //Trace back the path from the given node
+    private void printaCaminho(Node node) {
+        if (node.parent != null)
+            printaCaminho(node.parent);
+        System.out.println("(X: " + node.x + ", Y:" + node.y + ")");
+    }
+
+
     // Classe Node
-    static class Node
-    {
+    private class Node implements Comparable<Node> {
         int x;
         int y;
         int dist;
+        int heuristica;
         Node parent;
 
         public void setParent(Node parent){
             this.parent = parent;
         }
-        Node(int x, int y, int dist, Node parent)
+        Node(int x, int y, int dist, int heuristica, Node parent)
         {
             this.x = x;
             this.y = y;
             this.dist = dist;
+            this.heuristica = heuristica;
             this.parent = parent;
+        }
+
+        public int compareTo(Node other){
+            return (this.dist + this.heuristica) - (other.dist + other.heuristica);
         }
     };
 
@@ -141,8 +129,8 @@ public class AStarPathFinder
                 };
 
         // Busca o menor caminho saindo de (0, 0) ate
-        // o destino (7, 5)
-
-        //BuscaAStar(mat, 0, 0, 5, 5);
+        // o destino (5, 5)
+        AStarPathFinder asfind = new AStarPathFinder();
+        asfind.AStarPathFinder(mat, 10, 10, 0, 0, 5, 5);
     }
 }
